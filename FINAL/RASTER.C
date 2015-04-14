306d04977c18ea489a730b9ca46dca2a9d29fdd0
Louis Mau <lmau404@mtroyal.ca>
2015-04-14 14:56:47 -0600

    Updated stage2
    Made Tst_rast basically do what render initial frame should do.

--- FINAL/RASTER.C
+++ FINAL/RASTER.C
@@ -46,3 +46,40 @@ void plot_bitmap_32(UINT32 *base, int x, int y, const UINT32 *bitmap, unsigned i
 		}
 	}
 }
+
+void plot_ver_line(UINT8 *base, int x, int y, int length)
+{
+	int i;
+	int sum = 0;
+	
+	if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT){
+		base +=  y * 80+ (x >> 3);
+		
+		for (i = 0; i < length; i++)
+		{
+			if((sum+y) <= SCREEN_HEIGHT){
+				*(base + i*80) |= 1 << 7 - (x & 7);
+				sum++;
+				}
+		}
+	}
+
+}
+
+void paintRgn(UINT32 *base,int x, int y, int width, int height)
+{
+	int i;
+	int j;
+	
+		if (x >= 0 && x < SCREEN_WIDTH && y >= 0 && y < SCREEN_HEIGHT){
+			base +=  y * 20+ (x >> 5);
+	
+		for(i = 0; i < height; i++)
+		{
+			for(j = 0; j < width; j++)
+			{
+				*(base + i*20 + j) = 0xFFFFFFFF;
+			}
+		}
+	}
+}
--- FINAL/TST_RAST.C
+++ FINAL/TST_RAST.C
@@ -8,12 +8,19 @@ int main()
 	int x, y;
 
 	clrScrn(base);
-	plot_bitmap_32(base, 50, 50, asian_facing_right_bitmap, ASIAN_FACING_RIGHT);
-	plot_bitmap_32(base, 100, 50, asian_facing_left_bitmap, ASIAN_FACING_RIGHT);
-	plot_bitmap_32(base, 150, 50, trashcan_bitmap, TRASHCAN);
-	plot_bitmap_16(base, 200, 50, chopsticks_moving_left, CHOPSTICKS);
-	plot_bitmap_16(base, 250, 50, chopsticks_moving_right, CHOPSTICKS);
-	
+	plot_ver_line(base,320,0,640);
+	paintRgn(base,0,320,20,80);
+	plot_bitmap_32(base, 0, 130, asian_facing_right_bitmap, ASIAN_FACING_RIGHT);
+	plot_bitmap_32(base, 608, 130, asian_facing_left_bitmap, ASIAN_FACING_RIGHT);
+	plot_bitmap_32(base, 96, 32, trashcan_bitmap, TRASHCAN);
+	plot_bitmap_32(base, 192, 130, trashcan_bitmap, TRASHCAN);
+	plot_bitmap_32(base, 96, 224, trashcan_bitmap, TRASHCAN);
+	plot_bitmap_32(base, 512, 32, trashcan_bitmap, TRASHCAN);
+	plot_bitmap_32(base, 416, 130, trashcan_bitmap, TRASHCAN);
+	plot_bitmap_32(base, 512, 224, trashcan_bitmap, TRASHCAN);
+
+	Cconin();
+
 
 	return 0;
 }
