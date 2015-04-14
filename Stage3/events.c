ac8de561da6d94322622131b7ea58034989e8bee
Louis Mau <lmau404@mtroyal.ca>
2015-04-14 12:59:08 -0600

    house keeping

--- Stage3/events.c
+++ Stage3/events.c
@@ -4,39 +4,7 @@ void updateModelSync(struct Model *model)
 {
 	
 	
-	return;
-}
-
-void asyncstuff()
-{
 	
-	
-	
-	return;
-}
-
-void moveLeft(struct Model *model)
-{
-	asianMoveLeft(&model->asian1);
-	return;
-}
-void moveRight(struct Model *model)
-{
-	
-	return;
-}
-void moveUp(struct Model *model)
-{
-	
-	return;
-}
-void moveDown(struct Model *model)
-{
-	
-	return;
-}
-void shoot(struct Model *model)
-{
 	
 	return;
 }
