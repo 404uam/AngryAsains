bdfc64258bbea342162403790813c9c90df8ec27
Louis Mau <lmau404@E203-005.ad.mtroyal.ca>
2015-04-15 01:38:50 -0600

    fixing bitmaps
    Trash can!

--- FINAL/TST_RAST.C
+++ FINAL/TST_RAST.C
@@ -12,6 +12,8 @@ int main()
 	paintRgn(base,0,320,20,80);
 	plot_bitmap_32(base, 0, 130, asian_facing_right_bitmap, ASIAN_FACING_RIGHT);
 	plot_bitmap_32(base, 608, 130, asian_facing_left_bitmap, ASIAN_FACING_RIGHT);
+	plot_bitmap_16(base,70,70,chopsticks_moving_left,CHOPSTICKS);
+	plot_bitmap_16(base,140,70,chopsticks_moving_right,CHOPSTICKS);
 	plot_bitmap_32(base, 96, 32, trashcan_bitmap, TRASHCAN);
 	plot_bitmap_32(base, 192, 130, trashcan_bitmap, TRASHCAN);
 	plot_bitmap_32(base, 96, 224, trashcan_bitmap, TRASHCAN);
