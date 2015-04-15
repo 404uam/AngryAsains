8108db19c7efcbe869a36ecc50fa764967402a7a
Louis Mau <lmau404@E203-005.ad.mtroyal.ca>
2015-04-15 15:54:42 -0600

    updated stage 5 stage 4 files

--- Stage5/renderer.h
+++ Stage5/renderer.h
@@ -14,15 +14,16 @@ extern void plot_bitmap_32(UINT32 *base, int x, int y, const UINT32 *bitmap, uns
 extern void clrScrn(UINT32 *base);
 extern void plot_ver_line(UINT8 *base,int x, int y, int length);
 extern void paintRgn(UINT32 *base,int originX, int originY, int width, int height);
-extern void clrRgn(UINT32 *base,int x, int y, int width, int height);
+extern void clrRgn32(UINT32 *base,int x, int y, int width, int height);
+extern void clrRgn16(UINT16 *base,int x, int y, int width, int height);
 
 void render(const struct Model *model, UINT8 *base);
-void render_frame(UINT8 *base);
+void render_static_frame(UINT8 *base);
 void render_asian_facing_right(const struct Model *model, UINT32 *base);
 void render_asian_facing_left(const struct Model *model, UINT32 *base);
 void render_chopsticks_facing_right(const struct Chopstick *chopsticks, UINT16 *base);
 void render_chopsticks_facing_left(const struct Chopstick *chopsticks, UINT16 *base);
-/*void render_obstuction(const struct Model *model, UINT8 *base);*/
 void clrBitmap32(UINT32 *base,int x, int y);
+void clrBitmap16(UINT16 *base, int x, int y);
 
 #endif
