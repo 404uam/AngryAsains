fea2350180c6f8ebd7815715ea623670abb53961
Christopher Mah <github@christophermah.com>
2015-04-15 18:49:09 -0600

    Updates

--- Stage5/angryasians.c
+++ Stage5/angryasians.c
@@ -1,50 +1,43 @@
 #include "renderer.h"
 #include <osbind.h>
+#include <vt52.h>
+
+#define RETURN	0x001C000DL
+#define LEFT 	0x001E0061L
+#define RIGHT 	0x00200064L
+#define DOWN 	0x001F0073L
+#define UP 		0x00110077L
+#define SPACE 	0x00390020L
+
+UINT32 getTime();
 
 int main()
 {
 	void *base = Physbase();
-	long *timer = (long *)0x462; /* address of longword auto-inc’ed 70 x per s */
-	long timeNow;
+	UINT32 timeThen, timeNow, timeElapsed;
 
-	timeNow = *timer;
-
-	
-
-	struct Chopstick chopsticks;
-	
-	modelType model = {{288,0,5,1,true,0,0,{{310,50,1,1,false},{50,50,1,1,false},{0,0,1,1,false}}}, /*Asian 1*/
-					   {608,288,5,1,true,0,0,{{0,0,1,1,false},{0,0,1,1,false},{0,0,1,1,false}}}, /*Asian 2*/
-					   {{40,40},{40,60},{40,80}},											  /*Obstruction 1 */
-					   {{340,40},{340,60},{340,80}},										  /*Obstruction 2 */
+	modelType model = {{0,0,5,1,true,0,0,{{310,50,1,1,false},{50,50,1,1,false},{0,0,1,1,false}}}, 
+					   {608,288,5,1,true,0,0,{{0,0,1,1,false},{0,0,1,1,false},{0,0,1,1,false}}}, 
+					   {{40,40},{40,60},{40,80}},											
+					   {{340,40},{340,60},{340,80}},										
 					   {0,368}};
-				
-	render_frame(base);
+
+	render_static_frame(base);
 	render_asian_facing_right(&model,base);
 	render_asian_facing_left(&model,base);
 
-	Cconin();
-	
-	clrBitmap32(base,model.asian1.x,model.asian1.y);
-	Cconin();
-	clrBitmap32(base,model.asian2.x,model.asian2.y);
-	
-	chopsticks = model.asian1.chopsticks[0];
-	render_chopsticks_facing_right(&chopsticks,base);
-
 	bool quit = false;
+	timeThen = getTime();
 
-
-	while(quit) {
-		long timeThen, timeNow, timeElapsed;
-	
-		timeNow = get_time();
-		timeElapsed = timeNow – timeThen;
+	while(quit) 
+	{
+		timeNow = getTime();
+		timeElapsed = timeNow - timeThen;
 
 		long input;
 	
 		fflush (stdout);
-	
+
 		if(Cconis())
 		{
 			input = Cnecin();
@@ -72,12 +65,17 @@ int main()
 						break;
 				default:
 						break;
-			
 			}
 		}
 
 		if (timeElapsed > 0)
 		{
+			asianMoveModel(&model->asian1);
+
+			render_static_frame(base);
+			render_asian_facing_right(&model,base);
+			render_asian_facing_left(&model,base);
+			
 			timeThen = timeNow;
 		}
 	}
@@ -85,11 +83,12 @@ int main()
 	return 0;
 }
 
-long getTime() {
+UINT32 getTime() {
+	long *timer = (long *)0x462;
+	long timeNow;
 	long old_ssp;
 	old_ssp = Super(0); /* enter privileged mode */
 	timeNow = *timer;
 	Super(old_ssp); /* exit privileged mode as soon as possible */
-
 	return timeNow;
 }
