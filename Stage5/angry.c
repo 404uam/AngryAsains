4d479aa7cb74ce19750a3142fba5ba941418d058
Christopher Mah <github@christophermah.com>
2015-04-16 14:23:53 -0600

    cleanup

--- Stage5/angry.c
+++ Stage5/angry.c
@@ -13,25 +13,16 @@
 
 UINT32 getTime();
 
-/*
+
 UINT8 buffer[32256];
 UINT8 static_buffer[32256];
-<<<<<<< HEAD
 
 int main()
 {
 	bool isBase = true;
 	void *back = 0xFFFFFF00 & (long)buffer;
-=======
-*/
 
-int main()
-{
-	/*
-	void *back = buffer;
->>>>>>> 5fd4022e6b2e5093c41696138dbfdbab027cff74
 	void *background = static_buffer;
-	*/
 	void *base = Physbase();
 	UINT32 timeThen, timeNow, timeElapsed;
 	bool quit = false;
@@ -43,16 +34,11 @@ int main()
 					   {{512,32},{416,128},{512,224}},										
 					   {0,320}};
 					   
-	/*printf("/033f/n");*/
-	/*fflush(stdout);*/
-<<<<<<< HEAD
+
 
 	render_static_frame(background,&model);
 	render_static_frame(back,&model);
-=======
 	
-	/*render_static_frame(back);*/
->>>>>>> 5fd4022e6b2e5093c41696138dbfdbab027cff74
 	render_static_frame(base,&model);
 	render_asian_facing_right(&model,base);
 	render_asian_facing_left(&model,base);
@@ -106,7 +92,6 @@ int main()
 
 		if (timeElapsed > 0)
 		{
-<<<<<<< HEAD
 
 			if(isBase == true) {
 				unrender_alive_chopsticks(&model.asian1,back);
@@ -135,36 +120,17 @@ int main()
 				clrBitmap32(base,model.asian2.x,model.asian2.y);
 				updateModel(&model);
 				
-=======
-			/*memcpy(base, background, 32257);*/
-			unrender_alive_chopsticks(&model.asian1,base);
-			unrender_alive_chopsticks(&model.asian2,base);
-			ai(&model);
-			clrBitmap32(base,model.asian2.x,model.asian2.y);
-			updateModel(&model);
-			
->>>>>>> 5fd4022e6b2e5093c41696138dbfdbab027cff74
+
+				render_alive_chopsticks(&model.asian1,1,base);
+				render_alive_chopsticks(&model.asian2,2,base);
+				render_asian_facing_right(&model,base);
+				render_asian_facing_left(&model,base);
 
 
-			render_alive_chopsticks(&model.asian1,1,base);
-			render_alive_chopsticks(&model.asian2,2,base);
-			render_asian_facing_right(&model,base);
-			render_asian_facing_left(&model,base);
-
-
-<<<<<<< HEAD
 				memcpy(back, background, 32256);
 
 				isBase = true;
 			}
-			/*memcpy(base, background, 32257);*/
-			
-=======
-			/*
-			Setscreen(-1,back,-1);
-			Vsync();
-			*/
->>>>>>> 5fd4022e6b2e5093c41696138dbfdbab027cff74
 
 			timeThen = timeNow;
 		}
