fb57309ba1ec40482617ccc0f5675dfc300a87b7
Louis Mau <lmau404@mtroyal.ca>
2015-04-16 20:14:34 -0600

    stage 7 music.c

---
+++ Stage7/MUSIC.C
@@ -0,0 +1,20 @@
+#include "PSG.H"
+#include "MUSIC.H"
+  
+ void start_music()
+ {
+	 long old_ssp = Super(0);
+	 
+	 
+	 
+ }
+ 
+ 
+ void update_music(UINT32 time_elapsed)
+ {
+	 
+	 
+	 
+	 
+	 
+ }
