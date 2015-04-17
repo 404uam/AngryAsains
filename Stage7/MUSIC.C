e4ad05913b58d52bda04490df1bed31eae55a1f2
Louis Mau <lmau404@mtroyal.ca>
2015-04-16 21:44:26 -0600

    initial music

--- Stage7/MUSIC.C
+++ Stage7/MUSIC.C
@@ -1,10 +1,97 @@
 #include "PSG.H"
 #include "MUSIC.H"
-  
+
+struct Note noteChA[] ={
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{E,QRTR_NOTE,VOL},
+	{A,5,0},
+	{E,QRTR_NOTE,VOL},
+	{A,5,0},
+	{E,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{F,QRTR_NOTE,VOL},
+	{A,5,0},
+	{E,QRTR_NOTE,VOL},
+	{A,5,0},
+	{E,QRTR_NOTE,VOL},
+	{A,5,0},
+	{E,QRTR_NOTE,VOL},
+	{A,5,0},
+	{B,QRTR_NOTE,VOL},
+	{A,5,0},
+	{A,QRTR_NOTE,VOL},
+	{A,5,0}
+};
+struct Notes noteChB[] ={
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{G,QRTR_NOTE,VOL},
+	{A,5,0},
+	{B,QRTR_NOTE,VOL},
+	{A,5,0},
+	{B,QRTR_NOTE,VOL},
+	{A,5,0},
+	{B,QRTR_NOTE,VOL},
+	{A,5,0},
+	{B,QRTR_NOTE,VOL},
+	{A,5,0},
+	{B,QRTR_NOTE,VOL},
+	{A,5,0},
+	{B,QRTR_NOTE,VOL},
+	{A,5,0},
+	{C,QRTR_NOTE,VOL},
+	{A,5,0},
+	{C,QRTR_NOTE,VOL},
+	{A,5,0},
+	{C,QRTR_NOTE,VOL},
+	{A,5,0},
+	{A,QRTR_NOTE,0},
+	{A,5,0},
+	{A,QRTR_NOTE,0},
+	{A,5,0}
+};
+
  void start_music()
  {
 	 long old_ssp = Super(0);
 	 
+	 enable_channel(0,1,1);
+	 enable_channel(1,1,1);
 	 
 	 
  }
